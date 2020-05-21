016a56c6a2e5c7841d8c6603ee67859a286779ba
myloran <my.loran@gmail.com>
2020-05-21 13:56:37 +0300

    limit tiles on which the player can place his units - fix not tracking units in the board/bench - track coords in bench

--- Assets/Scripts/View/BenchView.cs
+++ Assets/Scripts/View/BenchView.cs
@@ -1,9 +1,12 @@
+using System.Collections.Generic;
 using Shared;
 using UnityEngine;
 
 namespace View {
   public class BenchView : MonoBehaviour, IUnitHolder {
     [SerializeField] Transform StartPoint;
+    public EPlayer Player { get; private set; }
+
     public EUnitHolder Type { get; } = EUnitHolder.Bench;
     
     void Start() {
@@ -12,18 +15,20 @@ namespace View {
       }
     }
 
-    public void Init(IUnitViewFactory unitFactory, TileViewFactory tileFactory) {
+    public void Init(IUnitViewFactory unitFactory, TileViewFactory tileFactory, EPlayer player) {
       this.unitFactory = unitFactory;
       this.tileFactory = tileFactory;
+      this.Player = player;
     }
 
     public float StartZPosition => StartPoint.position.z;
 
     public (bool, Coord) AddUnit(string name) {
       for (int x = 0; x < 10; x++) {
-        if (units[x] != null) continue;
+        var coord = new Coord(x, -1);
+        if (units.ContainsKey(coord)) continue;
 
-        units[x] = unitFactory.Create(name, TilePosition(x), tiles[x]);
+        units[coord] = unitFactory.Create(name, TilePosition(x), tiles[x], Player);
         return (true, new Coord(x, -1));
       }
 
@@ -32,10 +37,11 @@ namespace View {
     
     public (bool, Coord) RemoveUnit() {
       for (int x = 9; x >= 0; x--) {
-        if (units[x] == null) continue;
+        var coord = new Coord(x, -1);
+        if (!units.ContainsKey(coord)) continue;
         
-        Destroy(units[x].gameObject);
-        units[x] = null;
+        Destroy(units[coord].gameObject);
+        units.Remove(coord);
         return (true, new Coord(x, -1));
       }
       return (false, default);
@@ -53,10 +59,15 @@ namespace View {
     
     public void Place(UnitView unit, TileView tile) {
       unit.transform.position = TilePosition(tile.X).WithY(unit.Height);
+      units[new Coord(tile.X, tile.Y)] = unit;
+    }
+    
+    public void Unplace(UnitView unit, TileView tile) {
+      units.Remove(new Coord(tile.X, tile.Y));
     }
     
     readonly TileView[] tiles = new TileView[10];
-    readonly UnitView[] units = new UnitView[10];
+    readonly Dictionary<Coord, UnitView> units = new Dictionary<Coord, UnitView>(10);
     IUnitViewFactory unitFactory;
     TileViewFactory tileFactory;
   }
--- Assets/Scripts/View/BoardView.cs
+++ Assets/Scripts/View/BoardView.cs
@@ -1,8 +1,12 @@
+using System.Collections.Generic;
+using Shared;
 using UnityEngine;
+using static UnityEngine.Mathf;
 
 namespace View {
   public class BoardView : MonoBehaviour, IUnitHolder {
     [SerializeField] Transform StartPoint;
+
     public EUnitHolder Type { get; } = EUnitHolder.Board;
 
     void Start() {
@@ -18,12 +22,14 @@ namespace View {
       this.tileViewFactory = tileViewFactory;
     }
     
-    public TileView FindClosestTile(Vector3 position) {
+    public TileView FindClosestTile(Vector3 position, EPlayer selectedPlayer) {
       var indexPosition = position - StartPoint.position;
-      var indexX = Mathf.RoundToInt(indexPosition.x);
-      var indexY = Mathf.RoundToInt(indexPosition.z);
-      var x = Mathf.Clamp(indexX, 0, 7);
-      var z = Mathf.Clamp(indexY, 0, 5);
+      var indexX = RoundToInt(indexPosition.x);
+      var indexY = RoundToInt(indexPosition.z);
+      var x = Clamp(indexX, 0, 7);
+      var z = selectedPlayer == EPlayer.First
+        ? Clamp(indexY, 0, 2)
+        : Clamp(indexY, 3, 5);
 
       return tiles[x, z];
     }
@@ -32,9 +38,15 @@ namespace View {
 
     public void Place(UnitView unit, TileView tile) {
       unit.transform.position = TilePosition(tile.X, tile.Y).WithY(unit.Height);
+      units[new Coord(tile.X, tile.Y)] = unit;
+    }
+    
+    public void Unplace(UnitView unit, TileView tile) {
+      units.Remove(new Coord(tile.X, tile.Y));
     }
     
     readonly TileView[,] tiles = new TileView[8, 6];
+    readonly Dictionary<Coord, UnitView> units = new Dictionary<Coord, UnitView>(10);
     TileViewFactory tileViewFactory;
   }
 }
