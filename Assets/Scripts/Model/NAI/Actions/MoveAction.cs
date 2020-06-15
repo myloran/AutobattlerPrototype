9a41663b626c224241e4964e217b193091d39bbf
myloran <my.loran@gmail.com>
2020-06-15 17:47:53 +0300

    implement smart move

--- Assets/Scripts/Model/NAI/Actions/MoveAction.cs
+++ Assets/Scripts/Model/NAI/Actions/MoveAction.cs
@@ -40,27 +40,102 @@ namespace Model.NAI.Actions {
       var movement = Unit.Movement;
       var target = Unit.Target;
       var ai = Unit.Ai;
-      
-      var direction = (target.Unit.Movement.Coord - movement.Coord).Normalized;
+      var targetCoord = target.Unit.Movement.Coord;
+
+      var direction = (targetCoord - movement.Coord).Normalized;
       if (Move(context, movement, direction, ai, target)) return true;
 
       var (direction1, direction2) = direction.GetClosestDirections();
-      if (Move(context, movement, direction1, ai, target)) return true;
-      if (Move(context, movement, direction2, ai, target)) return true;
+      if (SmartMove(context, movement, direction1, direction2, targetCoord, ai, target)) return true;
 
       var direction3 = direction1.GetClosestDirection(direction);
-      if (Move(context, movement, direction3, ai, target)) return true;
-      
       var direction4 = direction2.GetClosestDirection(direction);
-      if (Move(context, movement, direction4, ai, target)) return true;
+      if (SmartMove(context, movement, direction3, direction4, targetCoord, ai, target)) return true;
       
       var direction5 = direction3.GetClosestDirection(direction1);
-      if (Move(context, movement, direction5, ai, target)) return true;
-      
       var direction6 = direction4.GetClosestDirection(direction2);
-      if (Move(context, movement, direction6, ai, target)) return true;
+      if (SmartMove(context, movement, direction5, direction6, targetCoord, ai, target)) return true;
 
       return false;
+    }
+
+    bool SmartMove(AiContext context, CMovement movement, Coord direction1, Coord direction2, Coord targetCoord, CAi ai,
+      CTarget target) {
+      var newCoord1 = movement.Coord + direction1;
+      var newCoord2 = movement.Coord + direction2;
+      var canMove1 = context.IsTileEmpty(newCoord1);
+      var canMove2 = context.IsTileEmpty(newCoord2);
+
+      if (canMove1 && canMove2) {
+        var (hasPos1, pos1) = CalculateNextMovePosition(context, newCoord1);
+        var (hasPos2, pos2) = CalculateNextMovePosition(context, newCoord2);
+
+        // if (hasPos1 && hasPos2) {
+          var distance1 = CoordExt.SqrDistance(targetCoord, pos1);
+          var distance2 = CoordExt.SqrDistance(targetCoord, pos2);
+
+          if (distance1.IsEqualTo(distance2)) {
+            if (hasPos1) return PureMove(newCoord2, direction2);
+            if (hasPos2) return PureMove(newCoord1, direction1);
+            
+            return PureMove(newCoord1, direction1);
+          }
+
+          if (distance1 < distance2)
+            return PureMove(newCoord1, direction1);
+          else
+            return PureMove(newCoord2, direction2);
+        // }
+        // else if (hasPos1) return PureMove(newCoord1, direction1);
+        // else if (hasPos2) return PureMove(newCoord2, direction2);
+      }
+      else if (canMove1) return PureMove(newCoord1, direction1);
+      else if (canMove2) return PureMove(newCoord2, direction2);
+
+      return false;
+          
+      bool PureMove(Coord newCoord, Coord direction) {
+        var time = movement.TimeToMove(direction.IsDiagonal);
+        new StartMoveCommand(context.Board, movement, newCoord, context.CurrentTime, time, Bus)
+          .Execute();
+      
+        context.InsertCommand(time, 
+          new EndMoveCommand(context.Board, movement, target, newCoord, Bus));
+      
+        context.InsertCommand(time, new MakeDecisionCommand(ai, context, time));
+        return true;
+      }
+    }
+
+    (bool, Coord) CalculateNextMovePosition(AiContext context, Coord coord) {
+      (bool, Coord) res = (false, coord);
+      
+      var direction = (Unit.Target.Unit.Movement.Coord - coord).Normalized;
+      if (TryMove(direction, ref res)) return res;
+
+      var (direction1, direction2) = direction.GetClosestDirections();
+      if (TryMove(direction1, ref res)) return res;
+      if (TryMove(direction2, ref res)) return res;
+
+      // var direction3 = direction1.GetClosestDirection(direction);
+      // var direction4 = direction2.GetClosestDirection(direction);
+      // if (TryMove(direction3, ref res)) return res;
+      // if (TryMove(direction4, ref res)) return res;
+      
+      // var direction5 = direction3.GetClosestDirection(direction1);
+      // var direction6 = direction4.GetClosestDirection(direction2);
+      // if (TryMove(direction5, ref res)) return res;
+      // if (TryMove(direction6, ref res)) return res;
+
+      return res;
+      
+      bool TryMove(Coord moveDirection, ref (bool, Coord) result) {
+        var pos = coord + moveDirection;
+        if (!context.IsTileEmpty(pos)) return false;
+        
+        result = (true, pos);
+        return true;
+      }
     }
 
     bool Move(AiContext context, CMovement movement, Coord direction, CAi ai, CTarget target) {
