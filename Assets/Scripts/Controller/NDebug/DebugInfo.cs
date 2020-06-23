fff1123ccac82921753a2c66860ad92c7156453f
myloran <my.loran@gmail.com>
2020-06-23 18:01:35 +0300

    use criptable object exntender only in debug info

--- Assets/Scripts/Controller/NDebug/DebugInfo.cs
+++ Assets/Scripts/Controller/NDebug/DebugInfo.cs
@@ -1,6 +1,8 @@
+using Addons.ScriptableObjectDrawer;
 using UnityEngine;
 
 namespace Controller.NDebug {
+  [UseExtendedScriptableObjectDrawer]
   [CreateAssetMenu(fileName = "DebugInfo", menuName = "Data/Debug", order = 1)]
   public class DebugInfo : ScriptableObject {
     public bool IsLogEnabled,
