f943b9484bef8709ecb89a9c16eaa83037d5ce30
myloran <my.loran@gmail.com>
2020-10-14 11:44:56 +0300

    move animations to humanoid

--- Assets/Scripts/View/NUnit/UnitViewFactory.cs
+++ Assets/Scripts/View/NUnit/UnitViewFactory.cs
@@ -9,19 +9,19 @@ namespace View.NUnit {
     public UnitViewFactory(Dictionary<string, UnitInfo> unitInfos, UnitViewInfoHolder unitViewInfoHolder, 
         CoordFinder coordFinder, Camera mainCamera) {
       this.unitInfos = unitInfos;
-      this._unitViewInfoHolder = unitViewInfoHolder;
+      this.unitViewInfoHolder = unitViewInfoHolder;
       this.coordFinder = coordFinder;
       this.mainCamera = mainCamera;
     }
 
     public UnitView Create(string name, Coord coord, EPlayer player) {
-      var unit = _unitViewInfoHolder.Prefab;
+      var unit = unitViewInfoHolder.Prefab;
       var position = coordFinder.PositionAt(coord).WithY(unit.Height);
       var rotation = player.ToQuaternion();
       var unitInfo = unitInfos[name];
       
       var obj = Object.Instantiate(unit, position, rotation);
-      var model = _unitViewInfoHolder.Infos[name].Model;
+      var model = unitViewInfoHolder.Infos[name].Model;
       Object.Instantiate(model, obj.transform);
       
       var healthBar = obj.GetComponentInChildren<HealthBar>()
@@ -32,7 +32,7 @@ namespace View.NUnit {
 
     readonly CoordFinder coordFinder;
     readonly Camera mainCamera;
-    readonly UnitViewInfoHolder _unitViewInfoHolder;
+    readonly UnitViewInfoHolder unitViewInfoHolder;
     readonly Dictionary<string, UnitInfo> unitInfos;
   }
 }
