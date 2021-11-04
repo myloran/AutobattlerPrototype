163c19dbe79b39121038a78fea8990dac38ef270
myloran <3658316+myloran@users.noreply.github.com>
2021-11-04 14:04:05 +0200

    load unit data recursively

--- Assets/Scripts/Infrastructure/UnitInfoLoader.cs
+++ Assets/Scripts/Infrastructure/UnitInfoLoader.cs
@@ -9,18 +9,29 @@ using UnityEngine;
 
 namespace Infrastructure {
   public class UnitInfoLoader {
+
     public Dictionary<string, UnitInfo> Load() {
-      var dataFolderPath = Path.Combine(Application.dataPath, "Data", "Units");
-      var files = Directory.GetFiles(dataFolderPath, "*.json");
-      var units = new Dictionary<string, UnitInfo>();
-
-      foreach (var file in files) {
-        var text = File.ReadAllText(file);
-        var unit = JsonConvert.DeserializeObject<UnitInfo>(text);
-        units[unit.Name] = unit;
-      }
-
+      var folderPath = Path.Combine(Application.dataPath, "Data", "Units");
+      ProcessDirectory(folderPath);
       return units;
     }
+
+    void ProcessDirectory(string folderPath) {
+      var files = Directory.GetFiles(folderPath, "*.json");
+      foreach (var file in files)
+        ProcessFile(file);
+
+      var subdirectoryEntries = Directory.GetDirectories(folderPath);
+      foreach (var subdirectory in subdirectoryEntries)
+        ProcessDirectory(subdirectory);
+    }
+    
+    void ProcessFile(string filePath) {
+      var text = File.ReadAllText(filePath);
+      var unit = JsonConvert.DeserializeObject<UnitInfo>(text);
+      units[unit.Name] = unit;
+    }
+
+    readonly Dictionary<string, UnitInfo> units = new Dictionary<string, UnitInfo>();
   }
 }
